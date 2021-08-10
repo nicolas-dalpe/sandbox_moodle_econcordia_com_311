12bb281fd39797f008bb0a04f837879a981de2b2
nicolas-dalpe <nicolas.dalpe@knowledgeone.ca>
2021-08-10 12:56:17 -0400

    local_loginas: Add lang string.

--- local/loginas/version.php
+++ local/loginas/version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_loginas';
 $plugin->release = '0.1.0';
-$plugin->version = 2021072904;
+$plugin->version = 2021081000;
 $plugin->requires = 2020061500;
 $plugin->maturity = MATURITY_ALPHA;
